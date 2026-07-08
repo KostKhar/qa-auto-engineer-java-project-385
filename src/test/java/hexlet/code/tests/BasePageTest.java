f971c7610a3d6dfc316dde1f743c962a7ae147a0
kostkhar <konstantin.kharik@onlinesup.com>
2026-07-08 20:08:16 +0300

    update browser

--- src/test/java/hexlet/code/tests/BasePageTest.java
+++ src/test/java/hexlet/code/tests/BasePageTest.java
@@ -3,19 +3,31 @@ package hexlet.code.tests;
 import hexlet.code.configure.Configuration;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import static hexlet.code.configure.ConfigurationManager.config;
 
 abstract class BasePageTest {
+    private static final Path SYSTEM_CHROMEDRIVER = Path.of("/usr/bin/chromedriver");
+    private static final Path SYSTEM_CHROMIUM = Path.of("/usr/bin/chromium");
+
     protected WebDriver driver;
 
     @BeforeAll
-    static void setupClass() {
+    static void setupBrowser() {
+        if (Files.exists(SYSTEM_CHROMEDRIVER)) {
+            System.setProperty("webdriver.chrome.driver", SYSTEM_CHROMEDRIVER.toString());
+            return;
+        }
+
         WebDriverManager.chromedriver().setup();
     }
 
@@ -28,7 +40,7 @@ abstract class BasePageTest {
     }
 
     @AfterEach
-    void tearDownAll() {
+    void tearDown() {
         if (driver != null) {
             driver.quit();
         }
@@ -39,10 +51,13 @@ abstract class BasePageTest {
 
         return switch (browser) {
             case "chromium", "chrome" -> {
-                WebDriverManager.chromedriver().setup();
                 ChromeOptions options = new ChromeOptions();
                 if (configuration.headless()) {
                     options.addArguments("--headless=new");
+                }
+                options.addArguments("--no-sandbox", "--disable-dev-shm-usage", "--disable-gpu");
+                if (Files.exists(SYSTEM_CHROMIUM)) {
+                    options.setBinary(SYSTEM_CHROMIUM.toString());
                 }
                 yield new ChromeDriver(options);
             }
