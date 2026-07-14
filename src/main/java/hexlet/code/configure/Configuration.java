5f39ae13130f8036710ce7e56d8e4a23f6c75b6a
kostkhar <konstantin.kharik@onlinesup.com>
2026-07-14 16:19:52 +0300

    fix

--- src/main/java/hexlet/code/configure/Configuration.java
+++ src/main/java/hexlet/code/configure/Configuration.java
@@ -4,14 +4,11 @@ import org.aeonbits.owner.Config;
 
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
-    "system:properties",
-    "classpath:config.properties",
-    "classpath:allure.properties"
+        "system:properties",
+        "classpath:config.properties",
+        "classpath:allure.properties"
 })
 public interface Configuration extends Config {
-
-    @Key("APP_BASE_URL")
-    String baseUrl();
 
     @Key("headless")
     boolean headless();
